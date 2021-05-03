a609bdd1b6854373376b7feffdfae1327fc5c5f3
CS_Balotiya <74857569+csbalotiya@users.noreply.github.com>
2021-05-04 00:17:23 +0530

    Create calculasFormulas.java

---
+++ app/java/com.example.minorproject/calculasFormulas.java
@@ -0,0 +1,17 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class calculasFormulas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculas_formulas);
+        Intent intent = getIntent();
+    }
+
+}
