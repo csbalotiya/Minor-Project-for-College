3e1ecaec7fba4f6b4ffc63b1850ba5afa81f9e6a
CS_Balotiya <74857569+csbalotiya@users.noreply.github.com>
2021-05-04 00:19:41 +0530

    Create trignometryFormula.java

---
+++ app/java/com.example.minorproject/trignometryFormula.java
@@ -0,0 +1,16 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class trignnometryFormulas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trignnometry_formulas);
+        Intent intent = getIntent();
+    }
+}
